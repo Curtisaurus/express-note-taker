f034ff2f51bf58d7095f2296b6e0c39cce9edbc1
Curtisaurus <ccdrullinger@gmail.com>
2021-06-15 00:21:00 -0400

    added res.end to post and delete calls

--- server.js
+++ server.js
@@ -43,6 +43,8 @@ app.post('/api/notes', (req, res) => {
             fs.writeFile(path.join(__dirname, '/db/db.json'), JSON.stringify(notesArr), 'utf8', (err) => {
                 if (err) {console.log(err)};
             });
+
+            res.end();
         }
     });
 });
@@ -63,6 +65,8 @@ app.delete('/api/notes/:id', (req, res) => {
         fs.writeFile(path.join(__dirname, '/db/db.json'), JSON.stringify(notesArr), 'utf8', (err) => {
             if (err) {console.log(err)};
         });
+
+        res.end();
     });
 });
 
